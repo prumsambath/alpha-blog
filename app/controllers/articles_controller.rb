5a1b7afbe988fddedcb15fa61b86c39c85068f9f
Sambath Prum <psb.sambath@gmail.com>
2017-05-17 18:29:07 -0500

    Authorize user to only modify their own account and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,15 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
     @article = Article.new
   end
 
   def create
-    byebug
     @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
       flash[:success] = 'Article was successfully created'
       redirect_to @article
@@ -48,5 +50,12 @@ class ArticlesController < ApplicationController
 
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      unless current_user == @article.user
+        flash[:danger] = 'You can only edit or delete your own articles'
+        redirect_to root_path
+      end
     end
 end
