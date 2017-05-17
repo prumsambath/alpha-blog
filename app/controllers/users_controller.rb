8807ca7235103e440c77faee27720148f945df77
Sambath Prum <psb.sambath@gmail.com>
2017-05-17 18:37:15 -0500

    Fix display welcome message after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the alpha blog #{@user.name}"
-      redirect_to articles_path
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to @user
     else
       render :new
     end
